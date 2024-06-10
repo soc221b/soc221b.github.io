cedbe05173abe3228b9272707bee988bca128f92
Ernest <>
2024-06-10 11:02:13 +0800

    feat: update github link

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -10,7 +10,7 @@
       </ul>
       <ul>
         <li>
-          <a href="https://github.com/iendeavor" target="_blank"> GitHub </a>
+          <a href="https://github.com/soc221b" target="_blank"> GitHub </a>
         </li>
         <li>
           <a href="https://www.npmjs.com/~iendeavor" target="_blank"> NPM </a>
