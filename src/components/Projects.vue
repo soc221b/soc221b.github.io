c438e430a59c9a00e1fe5933541763ce4b37fac9
Ernest <>
2024-09-14 22:12:48 +0800

    docs: update

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,12 +1,34 @@
 <script setup lang="ts">
-import Project from '@/components/Project.vue'
 import { projects } from '@/constants'
 </script>
 
 <template>
   <h2>Projects</h2>
 
-  <template v-for="project of projects" :key="project.label">
-    <Project :project="project" />
-  </template>
+  <div style="overflow-x: auto">
+    <table style="min-width: 360px">
+      <thead>
+        <tr>
+          <th>Project</th>
+          <th>Version</th>
+          <th>Downloads</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="project of projects" :key="project.name">
+          <td>
+            <a :href="project.href" target="_blank">
+              {{ project.name }}
+            </a>
+          </td>
+          <td>
+            <img :src="project.homeBadge" />
+          </td>
+          <td>
+            <img :src="project.downloadBadge" />
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
--- src/constants.ts
+++ src/constants.ts
@@ -1,11 +1,8 @@
 export type Project = {
   name: string
   href: string
-  description: string
-  status: string
-  skills: string[]
-  badges?: string[]
-  preview?: string
+  homeBadge: string
+  downloadBadge: string
 }
 
 export type Skill = {
@@ -15,55 +12,62 @@ export type Skill = {
 
 export const projects: Project[] = [
   {
-    name: '@import-meta-env/cli',
-    href: 'https://www.npmjs.com/package/@import-meta-env/cli',
-    description: 'Startup/Runtime environment variables solution for JavaScript.',
-    status: 'stable',
-    skills: ['java script', 'rust', 'unplugin', 'babel', 'swc', 'type script'],
-    badges: ['https://img.shields.io/npm/dw/@import-meta-env/cli?labelColor=13171F&color=27292F'],
-    preview: 'import-meta-env.png'
-  },
-  {
     name: 'Inspect Element',
     href: 'https://chrome.google.com/webstore/detail/flgcpmeleoikcibkiaiindbcjeldcogp',
-    description: 'Chrome extension for inspecting element for content, and margin.',
-    status: 'stable',
-    skills: ['java script'],
-    badges: [
+    homeBadge:
+      'https://img.shields.io/chrome-web-store/v/flgcpmeleoikcibkiaiindbcjeldcogp?labelColor=13171F&color=27292F',
+    downloadBadge:
       'https://img.shields.io/chrome-web-store/users/flgcpmeleoikcibkiaiindbcjeldcogp?labelColor=13171F&color=27292F'
-    ]
-  },
-  {
-    name: 'pinia-plugin-persistedstate-2',
-    href: 'https://www.npmjs.com/package/pinia-plugin-persistedstate-2',
-    description: 'Persist and rehydrate your Pinia state between page reloads.',
-    status: 'stable',
-    skills: ['java script', 'vue', 'pinia'],
-    badges: [
-      'https://img.shields.io/npm/dw/pinia-plugin-persistedstate-2?labelColor=13171F&color=27292F'
-    ],
-    preview: 'pinia-plugin-persisted-state-2.png'
-  },
-  {
-    name: 'yup-schema-faker',
-    href: 'https://www.npmjs.com/package/yup-schema-faker',
-    description: 'Fake data generator for yup.',
-    status: 'stable',
-    skills: ['java script'],
-    badges: ['https://img.shields.io/npm/dw/yup-schema-faker?labelColor=13171F&color=27292F']
   },
   {
     name: 'gRPC devtools',
     href: 'https://chrome.google.com/webstore/detail/fohdnlaeecihjiendkfhifhlgldpeopm',
-    description: 'Browser DevTools extension for debugging gRPC network requests.',
-    badges: [
+    homeBadge:
+      'https://img.shields.io/chrome-web-store/v/fohdnlaeecihjiendkfhifhlgldpeopm?labelColor=13171F&color=27292F',
+    downloadBadge:
       'https://img.shields.io/chrome-web-store/users/fohdnlaeecihjiendkfhifhlgldpeopm?labelColor=13171F&color=27292F'
-    ],
-    skills: ['java script', 'react', 'tailwindcss'],
-    status: 'stable',
-    preview: 'grpc-devtools.png'
+  },
+  {
+    name: 'Find',
+    href: 'https://chromewebstore.google.com/detail/find/akaeoepndhnhffnginkbdcoigbpnnljh',
+    homeBadge:
+      'https://img.shields.io/chrome-web-store/v/akaeoepndhnhffnginkbdcoigbpnnljh?labelColor=13171F&color=27292F',
+    downloadBadge:
+      'https://img.shields.io/chrome-web-store/users/akaeoepndhnhffnginkbdcoigbpnnljh?labelColor=13171F&color=27292F'
+  },
+  {
+    name: '@import-meta-env/cli',
+    href: 'https://www.npmjs.com/package/@import-meta-env/cli',
+    homeBadge: 'https://img.shields.io/npm/v/@import-meta-env/cli?labelColor=13171F&color=27292F',
+    downloadBadge:
+      'https://img.shields.io/npm/dw/@import-meta-env/cli?labelColor=13171F&color=27292F'
+  },
+  {
+    name: 'pinia-plugin-persistedstate-2',
+    href: 'https://www.npmjs.com/package/pinia-plugin-persistedstate-2',
+    homeBadge:
+      'https://img.shields.io/npm/v/pinia-plugin-persistedstate-2?labelColor=13171F&color=27292F',
+    downloadBadge:
+      'https://img.shields.io/npm/dw/pinia-plugin-persistedstate-2?labelColor=13171F&color=27292F'
+  },
+  {
+    name: 'yup-schema-faker',
+    href: 'https://www.npmjs.com/package/yup-schema-faker',
+    homeBadge: 'https://img.shields.io/npm/v/yup-schema-faker?labelColor=13171F&color=27292F',
+    downloadBadge: 'https://img.shields.io/npm/dw/yup-schema-faker?labelColor=13171F&color=27292F'
+  },
+  {
+    name: '@runtime-env/cli',
+    href: 'https://www.npmjs.com/package/@runtime-env/cli',
+    homeBadge: 'https://img.shields.io/npm/v/@runtime-env/cli?labelColor=13171F&color=27292F',
+    downloadBadge: 'https://img.shields.io/npm/dw/@runtime-env/cli?labelColor=13171F&color=27292F'
+  },
+  {
+    name: 'zod-schema-faker',
+    href: 'https://www.npmjs.com/package/zod-schema-faker',
+    homeBadge: 'https://img.shields.io/npm/v/zod-schema-faker?labelColor=13171F&color=27292F',
+    downloadBadge: 'https://img.shields.io/npm/dw/zod-schema-faker?labelColor=13171F&color=27292F'
   }
-].map((project) => ({
-  ...project,
-  skills: project.skills.map((skill) => skill.toLowerCase())
-}))
+]
+  .sort((a, b) => a.name.localeCompare(b.name))
+  .sort((a, b) => a.homeBadge.localeCompare(b.homeBadge))
